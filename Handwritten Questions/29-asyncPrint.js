83bfcd99a6c0f5a0ca2effcbfdfc7fd91ca9a2f4
Felicityty <felicity_1213@163.com>
2023-10-20 22:28:50 +0800

    docs updated: 2023-10-20（mixed）

--- Handwritten Questions/29-asyncPrint.js
+++ Handwritten Questions/29-asyncPrint.js
@@ -1,19 +1,17 @@
 /*
  * @Author: Felicity💪
  * @Date: 2023-09-17 22:42:14
- * @LastEditTime: 2023-10-11 21:20:39
+ * @LastEditTime: 2023-10-18 23:56:07
  */
 
 const sleep = function (delay) {
-  return new Promise((resolve, reject) => {
-    setTimeout(resolve, delay)
-  })
+  return new Promise((resolve, reject) => setTimeout(resolve, delay))
 }
 
 const start = async function () {
   for (let i = 0; i < 10; i++) {
-    let res = await sleep(1000)
-    console.log(res)
+    await sleep(1000)
+    console.log(i)
   }
 }
 
